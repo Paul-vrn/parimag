d3c9614dadbe017e380c1d2922031fb814123061
Paul-vrn <paul.vernin@gmail.com>
2022-02-05 00:50:52 +0100

    Modification de la navbar & reste probleme avec le dropdown

--- src/components/main/liste/liste.svelte
+++ src/components/main/liste/liste.svelte
@@ -3,7 +3,6 @@ import { onMount } from 'svelte';
 import { getListe } from '../../../api/getListe'
 import Membre from './membre/membre.svelte'
 import { TabContent, TabPane } from 'sveltestrap';
-
 let liste = []
 onMount(async () => {
     const res = await getListe();
@@ -11,10 +10,11 @@ onMount(async () => {
 	console.log(liste)
 });
 
+console.log("test")
 </script>
 <main id="liste">
 	<h1 class="text-center">liste de la Benzimag</h1>
-	<TabContent>
+	<TabContent id="test" >
 		{#each liste as pole, index}
 			<TabPane tabId={index} tab={pole.pole} active={!index}>
 				{#each pole.membres as membre, index}
@@ -25,3 +25,9 @@ onMount(async () => {
 	</TabContent>
 
 </main>
+<style>
+:global(#liste ul.nav) {
+    display: flex;
+    justify-content: center;
+}
+</style>

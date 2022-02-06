8b2f6f69e5a28b55a923cd10aa7edabbc6d944a9
Paul-vrn <paul.vernin@gmail.com>
2022-02-06 13:37:56 +0100

    Page d'accueil terminé mais pas responsive

--- src/components/main/about/about.svelte
+++ src/components/main/about/about.svelte
@@ -2,25 +2,12 @@
 
 </script>
 <main id="about">
-	<h1>À propos du site</h1>
+	<h1 class="text-center">À propos du site</h1>
 
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
+	
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
--- src/components/main/main.svelte
+++ src/components/main/main.svelte
@@ -15,34 +15,35 @@ import {
     function handleUpdate(event) {
         isOpen = event.detail.isOpen;
     }
+
     import { viewMain } from '../../stores'
     
-    function changeView(element){
-        $viewMain = element.target.id
-    }
-
-	function hover(el){
-		console.log(el.target)
+    function changeView(event){
+        $viewMain = event.target.id
+	}
+	function dropdownOpen(event){
+		if (!$viewMain.includes('drop'))
+			$viewMain = $viewMain + ' ' + 'drop'
 	}
 </script>
 
 
 <header>
-    <Navbar color="light" light expand="md" class="m-0 bg-white">
-		<NavbarBrand href="/" on:click={changeView}>
+    <Navbar color="light" light expand="md" class="m-0 p-0 bg-white fixed-top shadow-lg">
+		<NavbarBrand href="/" on:click={changeView} class="m-3">
 			<Image alt="Logo" src={'images/logo.png'} id="accueil" class="img-fluid" width="150"/>
 		</NavbarBrand>
 		<NavbarToggler on:click={() => (isOpen = !isOpen)} />
 		<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-		  <Nav class="ms-auto" navbar>
-			<NavItem>
-				<NavLink id="accueil" on:click={changeView}>Accueil</NavLink>
+		  <Nav class="d-flex w-100 justify-content-end" navbar>
+			<NavItem  class="mx-3 rounded-2">
+				<NavLink id="accueil" on:click={changeView} class={($viewMain=='accueil'?'onHit':'')}>Accueil</NavLink>
 			  </NavItem>
-			  <NavItem>
-				  <NavLink id="liste" on:click={changeView}>Liste</NavLink>
+			  <NavItem class="mx-3 rounded-2">
+				  <NavLink id="liste" on:click={changeView} class={($viewMain=='liste'?'onHit':'')}>Liste</NavLink>
 			  </NavItem>
-			<Dropdown  nav inNavbar data-bs-toggle="dropdown">
-			  <DropdownToggle nav caret>SOS</DropdownToggle>
+			<Dropdown  nav inNavbar data-bs-toggle="dropdown"  class="mx-3 rounded-top">
+			  <DropdownToggle nav caret on:click={dropdownOpen}>SOS</DropdownToggle>
 			  <DropdownMenu right>
 				<DropdownItem id="menus" on:click={changeView}>Menus</DropdownItem>
 				<DropdownItem id="services" on:click={changeView}>Services</DropdownItem>
@@ -50,45 +51,56 @@ import {
 				<DropdownItem id="commandes" on:click={changeView}>Suivi de commande</DropdownItem>
 			  </DropdownMenu>
 			</Dropdown>			
-			  <NavItem class="me-4">
-				  <NavLink id="about" on:click={changeView}>À propos</NavLink>
+			  <NavItem class="mx-3 rounded-2">
+				  <NavLink id="about" on:click={changeView} class={($viewMain=='about'?'onHit':'')}>À propos</NavLink>
 			  </NavItem>
 		  </Nav>
 		</Collapse>
 	  </Navbar>
 </header>
-{#if $viewMain == 'accueil'}
+{#if $viewMain.includes('accueil')}
 <Accueil/>
-{:else if $viewMain == 'liste'}
+{:else if $viewMain.includes('liste')}
 <Liste/>
-{:else if $viewMain == 'sos'}
-<Sos/>
-{:else if $viewMain == 'about'}
+{:else if $viewMain.includes('about')}
 <About/>
-{:else if $viewMain == 'menus'}
+{:else if $viewMain.includes('menus')}
 <Menus/>
-{:else if $viewMain == 'services'}
+{:else if $viewMain.includes('services')}
 <Services/>
-{:else if $viewMain == 'commandes'}
+{:else if $viewMain.includes('commandes')}
 <Commandes/>
 {/if}
 
 
 <style>
-:global(header li) {
-    background-color: white;
-    border-radius: 3px;
-    background: linear-gradient(to left, white 51%, #D7C378 50%) right;
-    background-size: 200%;
-    transition: .5s ease-out;
-    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+:global(.onHit){
+	background-color: #D7C378 !important;
+	border-radius: 3px;
+}
+:global(header li a){
+	text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+:global(header li:hover){
+	animation: liKeyFrame 1s forwards;
 }
 
-:global(li:hover) {
-    background-position: left;
+@keyframes liKeyFrame {
+	from {
+		background-color: white;
+	}
+	to {
+		background-color: #D7C378;
+	}
 }
 
-:global(li .dropdown-menu) {
-    background: #D7C378;
+:global(div.dropdown-menu.show){
+	animation: liKeyFrame 0.1s forwards;
+}
+
+@media (max-width: 640px) {
+	:global(header li){
+		text-align: center;
+	}
 }
 </style>

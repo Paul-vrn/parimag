be732388fa930e422a9cf732cbdda05328cf74c2
Paul-vrn <paul.vernin@gmail.com>
2022-02-10 19:09:20 +0100

    dossier liste clear pour arthur

--- src/frontend/components/main/liste/liste.svelte
+++ src/frontend/components/main/liste/liste.svelte
@@ -2,30 +2,25 @@
 import { onMount } from 'svelte';
 import { getListe } from '../../../api/getListe'
 import Membre from './membre/membre.svelte'
-import { TabContent, TabPane } from 'sveltestrap';
-let liste = []
+let liste = [] // table qui contient le liste.json
 onMount(async () => {
     const res = await getListe();
     liste = res;
 	console.log(liste)
-	polecurrent= liste[0]
 });
-polecurrent.membres
-	let test = "test1"
-
-	pole_selected = 0
-
 
 </script>
 
 <main id="liste">
-	<h1 class="text-center">liste de la Benzimag {test}</h1>
-
+<!-- TODO : page de la liste -->
 </main>
-<style>
-:global(#liste ul.nav) {
-    display: flex;
-    justify-content: center;
+
+
+<style> 
+
+main#liste {
+	width: 100%;
+	height: 100vh;
 }
 
 </style>

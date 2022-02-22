f9354d39060d66d81a6ce7a86ff8bd87499b4679
Paul-vrn <paul.vernin@gmail.com>
2022-02-22 02:21:17 +0100

    plein de truc brf

---
+++ src/frontend/components/main/sos/menus/tab_commande.svelte
@@ -0,0 +1,101 @@
+<script>
+    import { onMount } from 'svelte';
+    import { getProduits } from '../../../../api/produit'
+    import Produit from './produit/produit.svelte'
+    import Service from './service/service.svelte'
+    import {TabContent, TabPane} from 'sveltestrap'
+    let produits = []
+    onMount(async () => {
+        const res = await getProduits();
+        produits = res;
+    });
+
+    export let updatePanier;
+    export let commandeEnCours;
+    function addInPanier(newProduit){
+        let existe = false
+        commandeEnCours.panier.forEach(produit => {
+            if (produit.id == newProduit.id){
+                produit.quantite = newProduit.quantite;
+                existe = true
+            }
+        });
+        if (!existe){
+            commandeEnCours.panier.push(newProduit)
+        }
+        existe = false
+        updatePanier(commandeEnCours.panier)
+    }
+</script>
+
+<div class="produits">
+    <TabContent pills>
+        <TabPane tabId="Plats" tab="Plats" active>
+            <div class="d-flex flex-wrap justify-content-around">
+                {#each produits.filter(prod => prod.type === "Repas") as produit}
+                    <Produit produit={produit} {addInPanier}/>
+                {/each}
+                
+            </div>
+        </TabPane>
+        <TabPane tabId="Service" tab="Services">
+            <div class="d-flex flex-wrap justify-content-around">
+                {#each produits.filter(prod => prod.type !== "Repas") as service}
+                    <Service service={service} {addInPanier}/>
+                {/each}
+            </div>
+        </TabPane>
+      </TabContent>
+</div>
+<style>
+:global(div.produits div.tab-content){
+    border-style: solid;
+    border-radius: 1em;
+    height: 100%;
+    min-height: 80%;
+}
+:global(div.produits div.tab-content ul){
+    align-items: center;
+    border-bottom-style: solid;
+}
+:global(div.produits div.tab-content ul li) {
+    width: 50%;
+}
+:global(div.produits div.tab-content ul li a){
+    border-top-left-radius: 1em;
+    border-top-right-radius: 1em;
+    text-align: center;
+    font-family: Paris2024Regular;
+    text-decoration-color: #D7C378;
+    color: #D7C378;
+
+}
+:global(div.produits div.tab-content ul li a.active){
+    background-color: #D7C378;
+}
+:global(div.tab-pane){
+    padding-top: 1em;
+    padding-bottom: 1em;
+}
+div.produits {
+    width: calc(100% - 550px);
+    margin-left: 0.5rem!important;
+}
+
+@media (min-width: 800px) and (max-width: 1015px){
+    div.produits {
+        width: calc(100% - 450px)
+    }
+}
+@media screen and (max-width: 800px) {
+    :global(div.tab-content){
+        width: calc(100% - 2rem);
+        margin-left: 1rem !important;
+        margin-right: 1rem !important;
+    }
+    div.produits {
+        width:100%;
+    }
+}
+
+</style>
